dde2ac8f7939fbe5e0072b19982eba3dbd31b008
araz-code <araz100@gmail.com>
2023-11-19 22:24:37 +0100

    add new urls for ipad and iphone distinction

--- shorts/urls.py
+++ shorts/urls.py
@@ -17,6 +17,19 @@ router.register('test/watch/sellers', ShortSellerView)
 router.register('test/watch/details', ShortedStockDetailsView)
 router.register('test/pick/details', ShortedStockDetailsView)
 
+router.register('iphone/pick', ShortedStockView)
+router.register('ipad/pick', ShortedStockView)
+router.register('iphone/watch', ShortedStockView)
+router.register('ipad/watch', ShortedStockView)
+router.register('iphone/test/pick', ShortedStockView)
+router.register('ipad/test/pick', ShortedStockView)
+router.register('iphone/test/watch', ShortedStockView)
+router.register('ipad/test/watch', ShortedStockView)
+router.register('iphone/test/watch/details', ShortedStockDetailsView)
+router.register('ipad/test/watch/details', ShortedStockDetailsView)
+router.register('iphone/test/pick/details', ShortedStockDetailsView)
+router.register('ipad/test/pick/details', ShortedStockDetailsView)
+
 
 urlpatterns = [
     path('', include(router.urls))
