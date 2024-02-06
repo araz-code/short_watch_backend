4ed9fe321f786c1a466700a9dedfcf46819cce63
araz-code <araz100@gmail.com>
2024-02-06 14:14:29 +0100

    try to get real IP

--- request_logging/middleware.py
+++ request_logging/middleware.py
@@ -4,10 +4,10 @@ from request_logging.models import RequestLog
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
 
-    if request.META.get('HTTP_X_REAL_IP'):
-        ip = request.META.get('HTTP_X_REAL_IP')
-    elif x_forwarded_for:
+    if x_forwarded_for:
         ip = x_forwarded_for.split(',')[0]
+    elif request.META.get('HTTP_X_REAL_IP'):
+        ip = request.META.get('HTTP_X_REAL_IP')
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
