9a10bb878387d47ed27adecfcf9ece870eb5f102
araz-code <araz100@gmail.com>
2024-02-06 11:42:24 +0100

    removed strict mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -46,9 +46,7 @@ const router = createHashRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>
 );
