bcb863281647314d553ec6bce989d30f8526eb58
araz-code <araz100@gmail.com>
2024-02-08 08:07:43 +0100

    change to dark mode

--- short_watch_backend/settings.py
+++ short_watch_backend/settings.py
@@ -214,8 +214,7 @@ JAZZMIN_UI_TWEAKS = {
     "sidebar_nav_compact_style": True,
     "sidebar_nav_legacy_style": False,
     "sidebar_nav_flat_style": False,
-    "theme": "default",
-    "dark_mode_theme": None,
+    "theme": "darkly",
     "button_classes": {
         "primary": "btn-primary",
         "secondary": "btn-secondary",
