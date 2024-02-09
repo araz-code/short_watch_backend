b1fed593b1b0431f8900a7616aabdcd98a75fc94
araz-code <araz100@gmail.com>
2024-02-09 16:19:33 +0100

    dark mode

--- frontend/src/routes/ErrorPage.tsx
+++ frontend/src/routes/ErrorPage.tsx
@@ -18,7 +18,7 @@ const ErrorPage: React.FC = () => {
   const error: unknown = useRouteError();
 
   return (
-    <div className="flex flex-col h-screen text-gray-800">
+    <div className="flex flex-col h-screen text-gray-800 dark:text-white">
       <Header />
       <div className="flex flex-col flex-grow items-center justify-center">
         <h1 className="text-3xl sm:text-4xl font-bold pb-5">Oops!</h1>
