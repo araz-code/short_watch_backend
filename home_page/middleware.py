d1e3cbc1ea5deb96d9e3a88032e96331edccec72
araz-code <araz100@gmail.com>
2024-02-11 18:21:08 +0100

    2 hour cache

--- home_page/middleware.py
+++ home_page/middleware.py
@@ -1,3 +1,6 @@
+from datetime import datetime, timedelta
+
+
 class CacheControlMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
@@ -7,7 +10,10 @@ class CacheControlMiddleware:
             return self.get_response(request)
 
         response = self.get_response(request)
-        response['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-        response['Pragma'] = 'no-cache'
-        response['Expires'] = '0'
+        # response['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+        # response['Pragma'] = 'no-cache'
+        # response['Expires'] = '0'
+
+        response['Cache-Control'] = 'public, max-age=7200'
+        response['Expires'] = (datetime.utcnow() + timedelta(seconds=7200)).strftime('%a, %d %b %Y %H:%M:%S GMT')
         return response
