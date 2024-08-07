2fdb46b68469703e0c5772fe90037e3ee2612b2b
araz-code <araz100@gmail.com>
2024-08-07 18:51:56 +0200

    look and feel of drop down menu

--- frontend/src/components/UI/DropDownMenu.tsx
+++ frontend/src/components/UI/DropDownMenu.tsx
@@ -17,7 +17,7 @@ const DropDownMenu: React.FC<{
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-blue-500 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 dark:hover:bg-[#aaaaaa]  dark:dark:bg-[#212121]">
+        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-blue-500 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 dark:hover:bg-gray-700 dark:bg-[#212121]">
           {t(selectedMenuItem) || "Symbol"}
           <FontAwesomeIcon
             icon={faChevronDown}
@@ -36,15 +36,20 @@ const DropDownMenu: React.FC<{
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 left-auto z-10 mt-2 w-50 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute right-0 left-5 z-10 mt-2 w-48 origin-top-right rounded-md bg-white dark:bg-[#121212] shadow-lg ring-1 ring-black dark:ring-white  ring-opacity-5 focus:outline-none">
+          <p className="py-3 px-3 border-b-[1.6px] border-gray-200 text-gray-600 dark:text-gray-300 text-base">
+            {t("Sort list by")}
+          </p>
           {options.map((option) => (
             <Menu.Item key={option}>
               {({ active }) => (
                 <button
                   onClick={() => onSelectMenuItemChange(option)}
                   className={classNames(
-                    active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                    "block px-4 py-2 text-sm w-full text-left"
+                    active
+                      ? "bg-gray-200 dark:bg-gray-400  text-black"
+                      : "text-black dark:text-white",
+                    "block px-4 py-2 text-base w-full text-left font-normal"
                   )}
                 >
                   {t(option)}
