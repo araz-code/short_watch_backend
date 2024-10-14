59d14e73a94b4f714609361555a780d207e0efc4
araz-code <araz100@gmail.com>
2024-10-14 23:20:00 +0200

    only date for large selling row

--- frontend/src/components/LargeShortSellingRow.tsx
+++ frontend/src/components/LargeShortSellingRow.tsx
@@ -21,7 +21,9 @@ const LargeShortSellingRow: React.FC<LargestShortSelling> = (props) => {
       <div className="border px-3 py-2 m-2 hover:bg-blue-100 text-sm dark:hover:bg-[#aaaaaa] dark:bg-[#212121] dark:text-white">
         <div className="font-medium text-wrap mb-2">{name}</div>
         <div className="grid grid-cols-2 place-content-between text-gray-500 dark:text-white">
-          <div className="font-medium text-left">{formatTimestamp(date)}</div>
+          <div className="font-medium text-left">
+            {formatTimestamp(date, "dateOnly")}
+          </div>
 
           <div className="flex items-center space-x-2 justify-end">
             {prevValue && (
