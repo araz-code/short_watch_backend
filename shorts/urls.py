bf3c29199b002b336411dd55171425a9669709cb
araz-code <araz100@gmail.com>
2025-01-28 21:14:14 +0100

    add new url paths for short sellers

--- shorts/urls.py
+++ shorts/urls.py
@@ -15,15 +15,15 @@ router.register('pick/details', ShortPositionDetailView)
 
 router.register('short-sellers', ShortSellerView)
 
-
 devices = ['iphone', 'ipad', 'iwatch', 'web']
 
-for category in ['pick', 'watch']:
-    for test_prefix in ['', 'test/']:
-        for device in devices:
+for test_prefix in ['', 'test/']:
+    for device in devices:
+        router.register(f'{device}/short-sellers', ShortSellerView)
+
+        for category in ['pick', 'watch']:
             router.register(f'{device}/{test_prefix}{category}', ShortPositionView)
             router.register(f'{device}/{test_prefix}{category}/details', ShortPositionDetailView)
-
 
 urlpatterns = [
     path('', include(router.urls))
