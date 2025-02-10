98ca38e9ac90fa080824fdbad3d437818c2ced90
araz-code <araz100@gmail.com>
2025-02-10 15:50:53 +0100

    add measurements

--- measurements/views.py
+++ measurements/views.py
@@ -1,10 +1,15 @@
-from rest_framework.decorators import api_view
+from django.db.transaction import atomic
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework_api_key.permissions import HasAPIKey
+
 from .serializers import MeasurementSerializer
 
 
 @api_view(['POST'])
+@permission_classes([HasAPIKey])
+@atomic
 def create_measurement(request):
     if request.method == 'POST':
         serializer = MeasurementSerializer(data=request.data)
