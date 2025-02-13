47ca902bb0eab4d9b15eeb87d5894941d986d9c4
araz-code <araz100@gmail.com>
2025-02-13 16:28:57 +0100

    measurements

--- measurements/urls.py
+++ measurements/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import AllMeasurementsAPI, LatestMeasurementAPI
 
 urlpatterns = [
     path('create/', views.create_measurement, name='create_measurement'),
+    path("", AllMeasurementsAPI.as_view(), name="all_measurements_api"),
+    path("latest/", LatestMeasurementAPI.as_view(), name="latest_measurement_api"),
 ]
--- measurements/views.py
+++ measurements/views.py
@@ -2,8 +2,10 @@ from django.db.transaction import atomic
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 from rest_framework_api_key.permissions import HasAPIKey
 
+from .models import Measurement
 from .serializers import MeasurementSerializer
 
 
@@ -17,3 +19,21 @@ def create_measurement(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AllMeasurementsAPI(APIView):
+    @staticmethod
+    def get(_):
+        measurements = Measurement.objects.order_by("-created_at")
+        serializer = MeasurementSerializer(measurements, many=True)
+        return Response(serializer.data)
+
+
+class LatestMeasurementAPI(APIView):
+    @staticmethod
+    def get(_):
+        measurement = Measurement.objects.order_by("-created_at").first()
+        if measurement:
+            serializer = MeasurementSerializer(measurement)
+            return Response(serializer.data)
+        return Response({"error": "No measurements found"}, status=status.HTTP_404_NOT_FOUND)
