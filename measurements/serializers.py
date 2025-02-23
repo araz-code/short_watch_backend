48a78cab3f8a5a360916dcac5c6c1f43661b2d93
araz-code <araz100@gmail.com>
2025-02-23 16:40:44 +0100

    add noise and light level

--- measurements/serializers.py
+++ measurements/serializers.py
@@ -6,7 +6,7 @@ class CreateMeasurementSerializer(serializers.ModelSerializer):
     userID = serializers.UUIDField(source='user_id')
     noiseLevel = serializers.FloatField(source='noise_level')
     lightLevel = serializers.IntegerField(source='light_level')
-    createdAt = serializers.DateTimeField(source='created_at', format='%Y-%m-%dT%H:%M:%S%z')
+    createdAt = serializers.DateTimeField(source='created_at', format='%Y-%m-%dT%H:%M:%S%z', required=False)
 
     class Meta:
         model = Measurement
