33bbf65b36b94ca8c39432c2d8e4d224bce4ef5d
araz-code <araz100@gmail.com>
2026-04-18 14:01:44 +0200

    Fix shaddow

--- frontend/src/components/Homepage/AppImage.tsx
+++ frontend/src/components/Homepage/AppImage.tsx
@@ -8,7 +8,7 @@ const AppImage: React.FC = () => {
   return (
     <div className="relative inline-block self-center">
       <img
-        className="max-h-[500px] w-auto self-center"
+        className="max-h-[500px] w-auto self-center drop-shadow-2xl"
         src={
           i18n.language === "da" || i18n.language === "da-DK"
             ? appDaImages
@@ -16,7 +16,7 @@ const AppImage: React.FC = () => {
         }
         alt="app images"
       />
-      <div className="absolute bottom-[-30px] left-1/2 transform -translate-x-1/2 w-[100%] h-2 bg-black opacity-60 rounded-full blur-lg"></div>
+      <div className="absolute bottom-[-25px] left-1/2 transform -translate-x-1/2 w-[80%] h-4 bg-black/35 rounded-[100%] blur-xl"></div>
     </div>
   );
 };
