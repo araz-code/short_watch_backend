fc02e0ef6c3fb4bdbc1c0328dbc25ba781fdef81
araz-code <araz100@gmail.com>
2026-04-18 18:59:34 +0200

    change app image.

--- frontend/src/components/Homepage/AppImage.tsx
+++ frontend/src/components/Homepage/AppImage.tsx
@@ -1,19 +1,11 @@
-import { useTranslation } from "react-i18next";
-import appEnImages from "../../static/app-en.png";
-import appDaImages from "../../static/app-da.png";
+import appImage from "../../static/app.png";
 
 const AppImage: React.FC = () => {
-  const { i18n } = useTranslation();
-
   return (
     <div className="relative inline-block self-center">
       <img
         className="max-h-[500px] w-auto self-center drop-shadow-2xl"
-        src={
-          i18n.language === "da" || i18n.language === "da-DK"
-            ? appDaImages
-            : appEnImages
-        }
+        src={appImage}
         alt="app images"
       />
       <div className="absolute bottom-[-25px] left-1/2 transform -translate-x-1/2 w-[80%] h-4 bg-black/35 rounded-[100%] blur-xl"></div>
